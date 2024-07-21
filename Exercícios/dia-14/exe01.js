58071c54203f0c28ae30dde710114e05902f1f67
Thalyson Lima da Silva <devthalyson72@gmail.com>
2024-07-21 18:11:50 -0300

    Dia 14
    Exercício do dia 14 ok.

--- Exercícios/dia-14/exe01.js
+++ Exercícios/dia-14/exe01.js
@@ -1,5 +1,63 @@
-let v = function(x){
-    return x * 2
+
+//PEGUNTAR O NOME O SALÁRIO 
+var nome 
+var salario
+function perguntarColaborador(){
+    nome = prompt('Informe o seu nome')
+    salario = parseFloat(prompt('Informe o seu salário atual:'))
+    calcularAumento(salario, nome)
 }
 
-console.log(v(5))
+//CALCULANDO O AUMENTO
+
+    let porcentagem = 0
+    if(salario <= 1500){
+        porcentagem = 20
+    } else if (salario <= 2000){
+        porcentagem = 15
+    } else if (salario <= 3000){
+        porcentagem = 10
+    } else {
+        porcentagem = 5
+    }
+
+    let novoSalario = salario + (salario * porcentagem / 100)
+    
+    //EXIBINDO AS INFORMAÇÕES NA TELA
+    console.log(`Nome: ${nome}\nSalário: R$${salario}\nTaxa de aumento: ${porcentagem}%\nSalário Atual: R$${novoSalario}`)
+
+    perguntarNovamente()
+}
+
+function perguntarNovamente(){
+    let novoCadastro = prompt('Digite 1 se deseja inserir novas informações ou 2 para encerrar!')
+    if(novoCadastro == 1){
+        perguntarColaborador()
+    } else {
+        console.log('Programa Encerrado!')
+    }
+}
+
+perguntarColaborador()
+
+
+ 
+
+
+
+
+/*MANEIRA OPCIONAL DE SE FAZER O CALCULO DO AUMENTO
+if(salario <= 1500){
+    novoSalario = salario + (salario * 20 / 100)
+    porcentagem = 20
+} else if ( salario <= 2000){
+    novoSalario = salario + (salario * 15 / 100)
+    porcentagem = 15
+} else if (salario <= 3000){
+    novoSalario = salario + (salario * 10 / 100)
+    porcentagem = 10
+} else {
+    porcentagem = 5
+    novoSalario = salario + (salario * 5 / 100)
+}
+*/
