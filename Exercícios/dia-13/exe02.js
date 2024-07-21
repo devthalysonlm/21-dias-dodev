fbca593dcd0922365fc8e93f6e521f47ebd2b993
Thalyson Lima da Silva <devthalyson72@gmail.com>
2024-07-21 15:41:02 -0300

    Update exe02.js

--- Exercícios/dia-13/exe02.js
+++ Exercícios/dia-13/exe02.js
@@ -9,7 +9,7 @@ class Carro {
         this.velocidadeMaxima = velocidadeMaxima
         this.aceleracao = aceleracao
     }
-    tempo(distancia=0){
+    Tempo(distancia=0){
         return distancia / (this.velocidadeMaxima / this.aceleracao)
     }
     apresentacao(){
@@ -32,7 +32,30 @@ class Corrida {
         this.vencedor = ''
 
     }
+    definirVencedor(){
+        let menorTempo = this.participantes[0].Tempo(this.distancia)
+        let vencedor = this.participantes[0]
 
+        for (let index = 1; index < this.participantes.length; index ++){
+            let tempo = this.participantes[index].Tempo(this.distancia)
+            if(tempo < menorTempo){
+                menorTempo = tempo
+                vencedor = this.participantes[index]
+            }
+        }
+        return this.vencedor = vencedor
+    
+    }
+    exibirVencedor(){
+        alert`O vencedor é ${this.vencedor.nome}`
+    }
 }
 
-let corrida1 = new Corrida('Interlagos', 'Fórmula1', 7000, )
+let corrida = new Corrida('Interlagos', 'Fórmula1', 7000, )
+
+corrida.participantes[0] = new Carro('Kia Sporatge', 180, 192)
+corrida.participantes[1] = new Carro('Palio Fire', 75, 157)
+corrida.participantes[2] = new Carro('Posche Carreira',650, 330 )
+
+corrida.definirVencedor()
+corrida.exibirVencedor()
