15ead976b656bf2d98d9c23898682b8896a1a8f4
Thalyson Lima da Silva <devthalyson72@gmail.com>
2024-07-21 18:36:43 -0300

    Dia 15

---
+++ Exercícios/dia-15/index.js
@@ -0,0 +1,22 @@
+let arrayA = [42,21,89,5,63,28,10,55,37,82]
+let arrayB = [47,21,36,59,14,83,27,60,92,45]
+let arrayC = [];
+function arrayDeSaida(arrayA,arrayB){
+    novoArray = []
+    novoArray2 = []
+    for(index = 0; index < arrayA; index++){
+        arrayA[index] = novoArray
+        
+    }
+
+    for(index2 = 0; index < arrayB; index2++){
+        arrayB[index2] = novoArray2
+    }
+    
+    if (arrayA[index] == arrayB[index2]){
+        arrayA[index] = arrayC
+
+    }
+    console.log(arrayC)
+}
+
