60460ab8d45b37575eeb7fb61b8079e2525ecfcb
Thalyson Lima da Silva <devthalyson72@gmail.com>
2024-10-25 16:50:26 -0300

    Update index.js

--- Exercícios/dia-17/index.js
+++ Exercícios/dia-17/index.js
@@ -1,40 +1,35 @@
+///////////////////////////HOTEL SAN REMO\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 let arrayHotel = []
 let arrayReserva = []
 
-function CadastrarHotel(){
-    let id = prompt('ID:')
-    let nome = prompt('Nome:')
-    let categoria = prompt('Categoria:')
-    let endereco = prompt('Endereço:')
-    let telefone = prompt('Telefone:')
 
+function CadastrarHotel(id, nome, categoria, endereco, telefone){
     let hotel = {
         id: id,
         nome: nome,
         categoria: categoria,
         endereco: endereco,
-        telefone: telefone,
-
+        telefone: telefone
     }
-    if(!arrayHotel.some(x => x.id === id))
+
     arrayHotel.push(hotel)
 }
 
-function CadastrarReserva(){
-    let id = prompt('ID:')
-    let idHotel = prompt('ID do Hotel:')
-    let nome = prompt('Nome do Responsável:')
-    let entrada = prompt('Dia de entrada:')
-    let saida = prompt('Dia de saída:')
-
+function CadastrarReserva(id, idHotel, responsavel, entrada, saida){
     let reserva = {
         id: id,
         idHotel: idHotel,
-        nome: nome,
+        responsavel: responsavel,
         entrada: entrada,
         saida: saida
     }
 
     arrayReserva.push(reserva)
+    
+}
+
+function ListagemDeReserva(idHotel){
+    return idHotel.filter(nome,)
+    
 }
