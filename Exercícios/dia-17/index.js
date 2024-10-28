a872da44b93b1818a2bf494e38dbc3c3b59dc71f
Thalyson Lima da Silva <devthalyson72@gmail.com>
2024-10-28 17:48:09 -0300

    Update index.js

--- Exercícios/dia-17/index.js
+++ Exercícios/dia-17/index.js
@@ -1,35 +1,49 @@
 ///////////////////////////HOTEL SAN REMO\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-let arrayHotel = []
-let arrayReserva = []
-
-
-function CadastrarHotel(id, nome, categoria, endereco, telefone){
-    let hotel = {
-        id: id,
-        nome: nome,
-        categoria: categoria,
-        endereco: endereco,
-        telefone: telefone
+class Hotel {
+    Id
+    Nome
+    Categoria
+    Endereco
+    Telefone
+    constructor(id, nome, categoria, endereco, telefone){
+        this.Id = id
+        this.Nome = nome
+        this.Categoria = categoria
+        this.Endereco = endereco
+        this.telefone = telefone
     }
-
-    arrayHotel.push(hotel)
+  
 }
 
-function CadastrarReserva(id, idHotel, responsavel, entrada, saida){
-    let reserva = {
-        id: id,
-        idHotel: idHotel,
-        responsavel: responsavel,
-        entrada: entrada,
-        saida: saida
+class Reserva {
+    Id
+    IdHotel
+    Responsavel
+    DiaEntrada
+    DiaSaida
+    constructor(id, idHotel, responsavel, diaEntrada, diaSaida){
+        this.Id = id
+        this.IdHotal = idHotel
+        this.Responsavel = responsavel
+        this.DiaEntrada = diaEntrada
+        this.DiaSaida = diaSaida
     }
-
-    arrayReserva.push(reserva)
-    
 }
 
-function ListagemDeReserva(idHotel){
-    return idHotel.filter(nome,)
-    
+let hoteis = []
+let reservas = []
+let idHotel = 1
+let idReserva = 1
+
+function CadastrarHotel(){
+    let nome = prompt('Informe o nome da unidade:')
+    let categoria = prompt('Informe a categoria:')
+    let endereco = prompt('Informe o endereço:')
+    let telefone = prompt('Informe o telefone para contato:')
+    let hotel = new Hotel(nome, categoria, endereco, telefone)
+    idHotel ++
+    hoteis.push(hotel)
+   
 }
+
